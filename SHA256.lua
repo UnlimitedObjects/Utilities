eee98d9ce6c815c36a65aed20e17caab16dd4aa7
UnlimitedObjects <122173987+UnlimitedObjects@users.noreply.github.com>
2024-06-21 18:49:45 -0500

    Update SHA256.lua

--- SHA256.lua
+++ SHA256.lua
@@ -1,4 +1,90 @@
-local Constants = {
+local MOD = 2^32
+local MODM = MOD-1
+
+local function memoize(f)
+	local mt = {}
+	local t = setmetatable({}, mt)
+	function mt:__index(k)
+		local v = f(k)
+		t[k] = v
+		return v
+	end
+	return t
+end
+
+local function make_bitop_uncached(t, m)
+	local function bitop(a, b)
+		local res,p = 0,1
+		while a ~= 0 and b ~= 0 do
+			local am, bm = a % m, b % m
+			res = res + t[am][bm] * p
+			a = (a - am) / m
+			b = (b - bm) / m
+			p = p*m
+		end
+		res = res + (a + b) * p
+		return res
+	end
+	return bitop
+end
+
+local function make_bitop(t)
+	local op1 = make_bitop_uncached(t,2^1)
+	local op2 = memoize(function(a) return memoize(function(b) return op1(a, b) end) end)
+	return make_bitop_uncached(op2, 2 ^ (t.n or 1))
+end
+
+local bxor1 = make_bitop({[0] = {[0] = 0,[1] = 1}, [1] = {[0] = 1, [1] = 0}, n = 4})
+
+local function bxor(a, b, c, ...)
+	local z = nil
+	if b then
+		a = a % MOD
+		b = b % MOD
+		z = bxor1(a, b)
+		if c then z = bxor(z, c, ...) end
+		return z
+	elseif a then return a % MOD
+	else return 0 end
+end
+
+local function band(a, b, c, ...)
+	local z
+	if b then
+		a = a % MOD
+		b = b % MOD
+		z = ((a + b) - bxor1(a,b)) / 2
+		if c then z = bit32_band(z, c, ...) end
+		return z
+	elseif a then return a % MOD
+	else return MODM end
+end
+
+local function bnot(x) return (-1 - x) % MOD end
+
+local function rshift1(a, disp)
+	if disp < 0 then return lshift(a,-disp) end
+	return math.floor(a % 2 ^ 32 / 2 ^ disp)
+end
+
+local function rshift(x, disp)
+	if disp > 31 or disp < -31 then return 0 end
+	return rshift1(x % MOD, disp)
+end
+
+local function lshift(a, disp)
+	if disp < 0 then return rshift(a,-disp) end 
+	return (a * 2 ^ disp) % 2 ^ 32
+end
+
+local function rrotate(x, disp)
+    x = x % MOD
+    disp = disp % 32
+    local low = band(x, 2 ^ disp - 1)
+    return rshift(x, disp) + lshift(low, 32 - disp)
+end
+
+local k = {
 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
 	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
 	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
@@ -17,86 +103,81 @@ local Constants = {
 	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 }
 
-local function Hexadecimal(String)
-	return (string.gsub(String, ".", function(Char)
-		return string.format("%02x", string.byte(Char))
-	end))
+local function str2hexa(s)
+	return (string.gsub(s, ".", function(c) return string.format("%02x", string.byte(c)) end))
 end
 
-local function NumberToBytes(x, y)
-	local String = ""
-	
-	for Idx = 1, y do
-		local Remainder = x % 256
-		String = string.char(Remainder) .. String
-		x = (x - Remainder) / 256
+local function num2s(l, n)
+	local s = ""
+	for i = 1, n do
+		local rem = l % 256
+		s = string.char(rem) .. s
+		l = (l - rem) / 256
 	end
-	
-	return String
+	return s
 end
 
-local function BytesToNumber(x, y)
-	local Number = 0
-	
-	for Idx = y, y + 3 do
-		Number = Number * 256 + string.byte(x, Idx)
-	end
-	
-	return Number
+local function s232num(s, i)
+	local n = 0
+	for i = i, i + 3 do n = n*256 + string.byte(s, i) end
+	return n
 end
 
-local function PreProcess(Message, Length)
-	local Extra = 64 - ((Length + 9) % 64)
-	Length = NumberToBytes(8 * Length, 8)
-	Message = Message .. "\128" .. string.rep("\0", Extra) .. Length
-	
-	return Message
+local function preproc(msg, len)
+	local extra = 64 - ((len + 9) % 64)
+	len = num2s(8 * len, 8)
+	msg = msg .. "\128" .. string.rep("\0", extra) .. len
+	assert(#msg % 64 == 0)
+	return msg
 end
 
-local function ProcessBlock(Message, Offset, Hash)
-	local W = {}
-	
-	for Idx = 1, 16 do
-		W[Idx] = BytesToNumber(Message, Offset + (Idx - 1) * 4)
-	end
-	
-	for Idx = 17, 64 do
-		local S0 = bit32.bxor(bit32.rrotate(W[Idx - 15], 7), bit32.rrotate(W[Idx - 15], 18), bit32.rshift(W[Idx - 15], 3))
-		local S1 = bit32.bxor(bit32.rrotate(W[Idx - 2], 17), bit32.rrotate(W[Idx - 2], 19), bit32.rshift(W[Idx - 2], 10))
-		W[Idx] = W[Idx - 16] + S0 + W[Idx - 7] + S1
-	end
-	
-	local A, B, C, D, E, F, G, H = table.unpack(Hash)
-	
-	for Idx = 1, 64 do
-		local S0 = bit32.bxor(bit32.rrotate(A, 2), bit32.rrotate(A, 13), bit32.rrotate(A, 22))
-		local MAJ = bit32.bxor(bit32.band(A, B), bit32.band(A, C), bit32.band(B, C))
-		local T2 = S0 + MAJ
-		local S1 = bit32.bxor(bit32.rrotate(E, 6), bit32.rrotate(E, 11), bit32.rrotate(E, 25))
-		local CH = bit32.bxor(bit32.band(E, F), bit32.band(bit32.bnot(E), G))
-		local T1 = H + S1 + CH + Constants[Idx] + W[Idx]
-		
-		H, G, F, E, D, C, B, A = G, F, E, D + T1, C, B, A, T1 + T2
-	end
-	
-	Hash[1] = bit32.band(Hash[1] + A)
-	Hash[2] = bit32.band(Hash[2] + B)
-	Hash[3] = bit32.band(Hash[3] + C)
-	Hash[4] = bit32.band(Hash[4] + D)
-	Hash[5] = bit32.band(Hash[5] + E)
-	Hash[6] = bit32.band(Hash[6] + F)
-	Hash[7] = bit32.band(Hash[7] + G)
-	Hash[8] = bit32.band(Hash[8] + H)
+local function initH256(H)
+	H[1] = 0x6a09e667
+	H[2] = 0xbb67ae85
+	H[3] = 0x3c6ef372
+	H[4] = 0xa54ff53a
+	H[5] = 0x510e527f
+	H[6] = 0x9b05688c
+	H[7] = 0x1f83d9ab
+	H[8] = 0x5be0cd19
+	return H
 end
 
-local function SHA256(String, Hash)
-	String = PreProcess(String, #String)
-	
-	for Idx = 1, #String, 64 do
-		ProcessBlock(String, Idx, Hash)
+local function digestblock(msg, i, H)
+	local w = {}
+	for j = 1, 16 do w[j] = s232num(msg, i + (j - 1)*4) end
+	for j = 17, 64 do
+		local v = w[j - 15]
+		local s0 = bxor(rrotate(v, 7), rrotate(v, 18), rshift(v, 3))
+		v = w[j - 2]
+		w[j] = w[j - 16] + s0 + w[j - 7] + bxor(rrotate(v, 17), rrotate(v, 19), rshift(v, 10))
 	end
-	
-	return Hexadecimal(NumberToBytes(Hash[1], 4) .. NumberToBytes(Hash[2], 4) .. NumberToBytes(Hash[3], 4) .. NumberToBytes(Hash[4], 4) .. NumberToBytes(Hash[5], 4) .. NumberToBytes(Hash[6], 4) .. NumberToBytes(Hash[7], 4) .. NumberToBytes(Hash[8], 4))
+
+	local a, b, c, d, e, f, g, h = H[1], H[2], H[3], H[4], H[5], H[6], H[7], H[8]
+	for i = 1, 64 do
+		local s0 = bxor(rrotate(a, 2), rrotate(a, 13), rrotate(a, 22))
+		local maj = bxor(band(a, b), band(a, c), band(b, c))
+		local t2 = s0 + maj
+		local s1 = bxor(rrotate(e, 6), rrotate(e, 11), rrotate(e, 25))
+		local ch = bxor (band(e, f), band(bnot(e), g))
+		local t1 = h + s1 + ch + k[i] + w[i]
+		h, g, f, e, d, c, b, a = g, f, e, d + t1, c, b, a, t1 + t2
+	end
+
+	H[1] = band(H[1] + a)
+	H[2] = band(H[2] + b)
+	H[3] = band(H[3] + c)
+	H[4] = band(H[4] + d)
+	H[5] = band(H[5] + e)
+	H[6] = band(H[6] + f)
+	H[7] = band(H[7] + g)
+	H[8] = band(H[8] + h)
 end
 
-return SHA256
+return function(msg)
+	msg = preproc(msg, #msg)
+	local H = initH256({})
+	for i = 1, #msg, 64 do digestblock(msg, i, H) end
+	return str2hexa(num2s(H[1], 4) .. num2s(H[2], 4) .. num2s(H[3], 4) .. num2s(H[4], 4) ..
+		num2s(H[5], 4) .. num2s(H[6], 4) .. num2s(H[7], 4) .. num2s(H[8], 4))
+end
